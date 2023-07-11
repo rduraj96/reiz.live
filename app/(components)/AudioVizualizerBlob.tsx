fb4178d7733d478c29d8e24e2e5b80efa2c4d211
rduraj96 <durajrei@gmail.com>
2023-07-10 22:23:13 -0400

    Updates Prism and layout

--- app/(components)/AudioVizualizerBlob.tsx
+++ app/(components)/AudioVizualizerBlob.tsx
@@ -1,7 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
 import * as THREE from "three";
-import { AsciiEffect, TrackballControls, OrbitControls } from "three-stdlib";
-import { createNoise3D } from "simplex-noise";
+import {
+  AsciiEffect,
+  TrackballControls,
+  OrbitControls,
+  EffectComposer,
+  RenderPass,
+  GlitchPass,
+  SSAOPass,
+  UnrealBloomPass,
+  BloomPass,
+  ClearPass,
+  FilmPass,
+  OutlinePass,
+  DotScreenPass,
+  ShaderPass,
+  RGBShiftShader,
+} from "three-stdlib";
+import { createNoise2D, createNoise3D } from "simplex-noise";
+import { RGBELoader } from "three-stdlib";
+import { SpotLight } from "@react-three/drei";
+import { AnimatePresence } from "framer-motion";
+import { GUI } from "dat.gui";
+import { Pause, Play } from "lucide-react";
 
 type AudioVizualizerBlobProps = {
   stationURL: string | null;
@@ -48,20 +69,24 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
 
   useEffect(() => {
     let camera: THREE.PerspectiveCamera;
-    let controls: TrackballControls;
+    // let controls: TrackballControls;
     let orbitControls: OrbitControls;
-    let scene: THREE.Scene | null = null;
+    let scene: THREE.Scene;
     let blobMesh: THREE.Mesh<
+      THREE.BufferGeometry,
+      THREE.Material | THREE.Material[]
+    >;
+    let boxMesh: THREE.Mesh<
       THREE.BufferGeometry,
       THREE.Material | THREE.Material[]
     >;
     let renderer: THREE.WebGLRenderer;
     let asciiEffect: AsciiEffect;
-    // let light1: THREE.PointLight,
-    //   light2: THREE.PointLight,
-    //   light3: THREE.PointLight,
-    //   light4: THREE.PointLight;
-    // const clock = new THREE.Clock();
+    let composer: EffectComposer;
+    let shaderMaterial: THREE.ShaderMaterial;
+    let glitchPass: GlitchPass;
+
+    // const gui = new GUI();
 
     const init = () => {
       const container = canvasRef.current;
@@ -71,9 +96,10 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
       const height = window.innerHeight;
 
       scene = new THREE.Scene();
-      scene.background = new THREE.Color(0, 0, 0);
+      scene.position.set(0, 0, 0);
+      // scene.background = new THREE.Color(0, 0, 0);
 
-      camera = new THREE.PerspectiveCamera(70, width / height, 1, 1000);
+      camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
       camera.position.set(0, 0, 100);
       camera.lookAt(scene.position);
       scene.add(camera);
@@ -81,47 +107,148 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
       renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
       renderer.setSize(width, height);
 
-      // Create custom blob geometry
-      const blobGeometry = new THREE.IcosahedronGeometry(29, 15);
-      const blobMaterial = new THREE.MeshLambertMaterial({
-        color: "rgb(84, 81, 81)",
+      renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+      // Create texture blob geometry
+      const texture = new THREE.DataTexture();
+      // const loader = new RGBELoader();
+      // loader.load("/texture/studio2.hdr", function (texture) {
+      //   texture.mapping = THREE.EquirectangularReflectionMapping;
+      //   // scene.background = texture;
+      //   scene.environment = texture;
+      // });
+      // blobMesh = new THREE.Mesh(
+      //   new THREE.SphereGeometry(10, 100, 100),
+      //   new THREE.MeshPhysicalMaterial({
+      //     roughness: 0.28,
+      //     metalness: 0.88,
+      //     transmission: 0,
+
+      //     ior: 2.33,
+      //     // envMap: texture,
+      //     envMapIntensity: 0.6,
+      //     clearcoat: 1,
+      //     color: 0xffffff,
+      //     reflectivity: 1,
+      //   })
+      // );
+      // scene.add(blobMesh);
+
+      // const boxGeometry = new THREE.BoxGeometry(100, 100, 100, 20, 20, 20);
+      // // gui.add()
+      // const boxMaterial = new THREE.MeshStandardMaterial({
+      //   color: 0x373937,
+      //   wireframe: true,
+      // });
+      // boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+      // boxMesh.position.set(0, 0, 0);
+      // scene.add(boxMesh);
+
+      // gui.add(boxMesh.position, "x", -150, 150);
+      // gui.add(boxMesh.position, "y", -150, 150);
+      // gui.add(boxMesh.position, "z", -150, 150);
+
+      const blobGeometry = new THREE.IcosahedronGeometry(10, 30);
+      const blobMaterial = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
         wireframe: true,
       });
       blobMesh = new THREE.Mesh(blobGeometry, blobMaterial);
       blobMesh.position.set(0, 0, 0);
       scene.add(blobMesh);
 
-      const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
       scene.add(ambientLight);
 
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-      directionalLight.position.set(1, 1, 1);
-      scene.add(directionalLight);
+      const dirLight1 = new THREE.DirectionalLight(0xffffff, 0.5);
+      dirLight1.position.set(1, 1, 50);
+
+      // const spotLight = new THREE.SpotLight(0xc3ff01);
+      // spotLight.intensity = 0.75;
+      // // spotLight.angle = 0.36;
+      // spotLight.position.set(-50, -30, 0);
+      // spotLight.lookAt(blobMesh.position);
+      // spotLight.penumbra = 1;
+      // spotLight.decay = 0.5;
+      // scene.add(spotLight);
+
+      // Create an EffectComposer
+      composer = new EffectComposer(renderer);
+
+      // Create a RenderPass and add it to the composer
+      const renderPass = new RenderPass(scene, camera);
+      composer.addPass(renderPass);
+
+      const effect2 = new ShaderPass(RGBShiftShader);
+      effect2.uniforms["amount"].value = 0.005;
+      composer.addPass(effect2);
+
+      //Create a GlitchPass and add it to the composer
+      glitchPass = new GlitchPass();
+      glitchPass.enabled = false;
+      glitchPass.curF = 0;
+      composer.addPass(glitchPass);
+
+      // const dotScreenPass = new DotScreenPass();
+      // dotScreenPass.enabled = true;
+      // composer.addPass(dotScreenPass);
+
+      // const filmPass = new FilmPass();
+      // composer.addPass(filmPass);
+
+      // const _SSAOPass = new SSAOPass(scene, camera);
+      // composer.addPass(_SSAOPass);
 
       // asciiEffect = new AsciiEffect(renderer, " .:-+*=%@#", { invert: true });
       // asciiEffect.setSize(window.innerWidth, window.innerHeight);
       // asciiEffect.domElement.style.color = "white";
       // asciiEffect.domElement.style.backgroundColor = "black";
 
-      controls = new TrackballControls(camera, renderer.domElement);
+      // controls = new TrackballControls(camera, renderer.domElement);
       orbitControls = new OrbitControls(camera, renderer.domElement);
       container.appendChild(renderer.domElement);
       window.addEventListener("resize", handleResize);
+      document.addEventListener("mousemove", onMouseMove, false);
+      document.addEventListener("mousedown", onMouseDown, false);
     };
 
     const handleResize = () => {
       const width = window.innerWidth;
       const height = window.innerHeight;
-
+      renderer.setSize(width, height);
+      composer.setSize(width, height);
       camera.aspect = width / height;
       camera.updateProjectionMatrix();
-      renderer.setSize(width, height);
     };
+
+    function onMouseDown(event: MouseEvent) {
+      // event.preventDefault();
+      glitchPass.enabled = true;
+      setTimeout(() => {
+        glitchPass.enabled = false;
+      }, 500);
+    }
+
+    function onMouseMove(event: MouseEvent) {
+      const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+      const mouseY = (event.clientY / window.innerHeight) * 2 - 1;
+
+      blobMesh.rotation.x = mouseY * 1.75;
+      blobMesh.rotation.y = mouseX * 1.5;
+    }
 
     const animate = () => {
       requestAnimationFrame(animate);
-      blobMesh.rotation.y += 0.005;
-      blobMesh.rotation.x += 0.005;
+      if (blobMesh) {
+        blobMesh.rotation.y += 0.005;
+        blobMesh.rotation.x += 0.005;
+      }
+
+      if (boxMesh) {
+        boxMesh.rotation.y += 0.001;
+        boxMesh.rotation.x += 0.001;
+      }
+
       render();
     };
 
@@ -130,25 +257,15 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
         const dataArray = new Uint8Array(analyserRef.current.frequencyBinCount);
         analyserRef.current.getByteFrequencyData(dataArray);
 
-        // const bass = getBassFrequency(dataArray);
-        // const treble = getTrebleFrequency(dataArray);
-
         const lowerHalfArray: number[] = Array.from(
           dataArray.slice(0, dataArray.length / 2 - 1)
         );
         const upperHalfArray: number[] = Array.from(
           dataArray.slice(dataArray.length / 2 - 1, dataArray.length - 1)
         );
-
-        // const overallAvg = avg(dataArray);
         const lowerMax = max(lowerHalfArray);
-        const lowerAvg = avg(lowerHalfArray);
-        const upperMax = max(upperHalfArray);
         const upperAvg = avg(upperHalfArray);
-
         const lowerMaxFr = lowerMax / lowerHalfArray.length;
-        const lowerAvgFr = lowerAvg / lowerHalfArray.length;
-        const upperMaxFr = upperMax / upperHalfArray.length;
         const upperAvgFr = upperAvg / upperHalfArray.length;
 
         makeRoughBall(
@@ -156,13 +273,10 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
           modulate(Math.pow(lowerMaxFr, 0.8), 0, 1, 0, 8),
           modulate(upperAvgFr, 0, 1, 0, 4)
         );
-
-        // makeRoughBall(blobMesh, bass, treble);
       }
-
-      controls.update();
+      // controls.update();
       orbitControls.update();
-      renderer.render(scene!, camera);
+      composer.render();
     };
 
     const simplex = createNoise3D();
@@ -176,9 +290,6 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
       const positions = geometry.getAttribute(
         "position"
       ) as THREE.BufferAttribute;
-      const boundingSphere = new THREE.Sphere();
-      geometry.computeBoundingSphere();
-      const offset = boundingSphere.radius;
       const amp = 7;
       const time = window.performance.now();
       const positionAttribute = geometry.getAttribute("position");
@@ -187,7 +298,7 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
         vertex.fromBufferAttribute(positionAttribute, i);
         vertex.normalize();
         const distance =
-          29 +
+          10 +
           bassFrequency +
           simplex(
             vertex.x + time * 0.00007,
@@ -201,23 +312,6 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
       }
       positions.needsUpdate = true;
       geometry.computeVertexNormals();
-    };
-
-    const getBassFrequency = (dataArray: Uint8Array) => {
-      const bassArray = dataArray.slice(0, dataArray.length / 4);
-      const bassSum = bassArray.reduce((sum, value) => sum + value, 0);
-      const bassAvg = bassSum / bassArray.length;
-      return bassAvg / 255; // Normalize the value between 0 and 1
-    };
-
-    const getTrebleFrequency = (dataArray: Uint8Array) => {
-      const trebleArray = dataArray.slice(
-        dataArray.length / 4,
-        dataArray.length
-      );
-      const trebleSum = trebleArray.reduce((sum, value) => sum + value, 0);
-      const trebleAvg = trebleSum / trebleArray.length;
-      return trebleAvg / 255; // Normalize the value between 0 and 1
     };
 
     function fractionate(val: number, minVal: number, maxVal: number): number {
@@ -250,21 +344,30 @@ const AudioVizualizerBlob: React.FC<AudioVizualizerBlobProps> = ({
 
     return () => {
       window.removeEventListener("resize", handleResize);
-      renderer.renderLists.dispose();
+      window.removeEventListener("mousemove", onMouseMove);
+      renderer.dispose();
     };
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-col justify-center">
       <div ref={canvasRef} />
-      {stationURL && (
-        <button
-          onClick={initAudio}
-          className="border p-3 bg-black text-white absolute z-10 top-2/3 left-1/2 rounded-lg"
-        >
-          {isPlaying ? "Pause" : "Play"}
-        </button>
-      )}
+      <AnimatePresence>
+        {stationURL && (
+          <div className="absolute z-10 left-1/2 bottom-0 mb-10 flex justify-between items-center gap-10 -translate-x-1/2 -translate-y-1/2">
+            <h3 className="text-white font-bold">PREV</h3>
+            <button
+              onClick={() => {
+                initAudio();
+              }}
+              className="hover:cursor-pointer text-white"
+            >
+              {isPlaying ? <Pause size={56} /> : <Play size={56} />}
+            </button>
+            <h3 className="text-white font-bold">NEXT</h3>
+          </div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
