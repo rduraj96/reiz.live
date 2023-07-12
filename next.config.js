8042b9b3a81ed748f643f3964a673aecbdf864d6
rduraj96 <durajrei@gmail.com>
2023-07-12 02:57:33 -0400

    Removed temp page

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/comingsoon',
-                permanent: true,
-            },
-        ]
-    },
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/',
+    //             destination: '/comingsoon',
+    //             permanent: true,
+    //         },
+    //     ]
+    // },
 }
 
 module.exports = nextConfig
