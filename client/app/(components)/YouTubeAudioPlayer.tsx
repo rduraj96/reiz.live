256ec501daf28e6997d5a07d98ee00311962e964
rduraj96 <durajrei@gmail.com>
2023-07-24 20:57:07 -0400

    Socket.io client-side implementation

--- client/app/(components)/YouTubeAudioPlayer.tsx
+++ client/app/(components)/YouTubeAudioPlayer.tsx
@@ -1,13 +1,20 @@
 import { Pause, Play } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { Slider } from "@/components/ui/slider";
+import { io, Socket } from "socket.io-client";
 
 type Playlist = {
   title: string;
   url: string;
 };
 
+type AudioState = {
+  currentTrackIndex: number;
+  currentTime: number;
+};
+
 const YouTubeAudioPlayer: React.FC = () => {
+  const socket = useRef<Socket | null>(null);
   const audioRef = useRef<HTMLAudioElement>(null);
   const [playlist, setPlaylist] = useState<Playlist[]>([]);
   const [isPlaying, setPlaying] = useState<boolean>(false);
@@ -15,6 +22,7 @@ const YouTubeAudioPlayer: React.FC = () => {
   const [duration, setDuration] = useState(0);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
   const [seekerValue, setSeekerValue] = useState(0);
+  const [connectedUsers, setConnectedUsers] = useState<string[]>([]);
 
   useEffect(() => {
     const getPlaylist = async () => {
@@ -40,7 +48,9 @@ const YouTubeAudioPlayer: React.FC = () => {
 
     if (audioElement) {
       audioElement.src = playlist[currentTrackIndex].url;
-      audioElement.play();
+      if (isPlaying) {
+        audioElement.play();
+      }
 
       const handleLoadedData = () => {
         setDuration(audioElement.duration);
@@ -59,26 +69,79 @@ const YouTubeAudioPlayer: React.FC = () => {
     }
   }, [playlist, currentTrackIndex]);
 
+  useEffect(() => {
+    socket.current = io("http://localhost:3001");
+
+    socket.current.on("connected-users", (users: string[]) => {
+      setConnectedUsers(users);
+      audioRef.current?.pause();
+      setPlaying(false);
+    });
+
+    socket.current.emit("listener-ready");
+
+    socket.current.on("get-audio-state", () => {
+      console.log(currentTrackIndex, currentTime);
+      socket.current?.emit("audio-state", {
+        currentTrackIndex,
+        currentTime,
+      });
+      console.log("Sent state", { currentTrackIndex });
+    });
+
+    socket.current.on("audio-state-from-server", (state: AudioState) => {
+      setCurrentTrackIndex(state.currentTrackIndex);
+      setCurrentTime(state.currentTime);
+    });
+
+    socket.current.on("play", () => {
+      audioRef.current?.play();
+      setPlaying(true);
+    });
+
+    socket.current.on("pause", () => {
+      audioRef.current?.pause();
+      setPlaying(false);
+    });
+
+    socket.current.on("track-change", (trackIndex) => {
+      setCurrentTrackIndex(trackIndex);
+    });
+
+    socket.current.on("seek", (time: number) => {
+      audioRef.current!.currentTime = time;
+      setCurrentTime(time);
+    });
+
+    return () => {
+      socket.current?.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {}, []);
+
   const playTrack = () => {
     audioRef.current?.play();
+    socket.current?.emit("play");
   };
 
   const pauseTrack = () => {
     audioRef.current?.pause();
+    socket.current?.emit("pause");
   };
 
   const playNextTrack = () => {
     if (currentTrackIndex < playlist.length - 1) {
-      setCurrentTrackIndex(currentTrackIndex + 1);
-      setSeekerValue(0);
+      const nextTrackIndex = currentTrackIndex + 1;
+      setCurrentTrackIndex(nextTrackIndex);
+      socket.current?.emit("track-change", nextTrackIndex);
     }
   };
 
   const playPreviousTrack = () => {
-    if (currentTrackIndex > 0) {
-      setCurrentTrackIndex(currentTrackIndex - 1);
-      setSeekerValue(0);
-    }
+    const nextTrackIndex = currentTrackIndex - 1;
+    setCurrentTrackIndex(nextTrackIndex);
+    socket.current?.emit("track-change", nextTrackIndex);
   };
 
   const handleEnded = () => {
@@ -95,15 +158,21 @@ const YouTubeAudioPlayer: React.FC = () => {
   const handleSeekerChange = (seek: number[]) => {
     const newSeekerValue = seek[0];
     setCurrentTime(newSeekerValue);
-
-    // Calculate the new time based on the seeker value
-    // const newTime = (newSeekerValue / 100) * duration;
     audioRef.current!.currentTime = newSeekerValue;
+    socket.current?.emit("seek", newSeekerValue);
   };
 
   return (
     playlist.length !== 0 && (
       <div className="relative space-y-4 mb-5">
+        <div className="text-white mb-10 text-center space-x-3">
+          <h4>Listener List:</h4>
+          {connectedUsers.map((user, i) => (
+            <div key={user} className="font-bold inline">
+              Listener {i + 1}
+            </div>
+          ))}
+        </div>
         <div className="text-center">
           {formatTime(currentTime)} / {formatTime(duration)}
         </div>
