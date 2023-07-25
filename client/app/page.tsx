6edb3bfff17bb4450ffa7c699885946e7da61a7c
rduraj96 <durajrei@gmail.com>
2023-07-24 20:56:51 -0400

    Separated darkside

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -64,171 +64,146 @@ export default function Home() {
 
   return (
     <div className="relative h-screen bg-foreground">
-      <AnimatePresence>
-        {showEgg ? (
-          <motion.div
-            key="easter-egg"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ delay: 0.5, duration: 1 }}
-            className="easter-egg"
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, type: "keyframes" }}
+      >
+        <AudioVizualizerBlob stationURL={currentStation?.urlResolved || null} />
+        {/* <AsciiEffectScene /> */}
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, type: "tween" }}
+        onClick={() => setShowEgg((prev) => !prev)}
+        className="hidden md:block absolute top-0 left-1/2 transform -translate-x-1/2 mt-20"
+      >
+        <h1
+          className="text-4xl text-gray-200 font-bold mb-6"
+          suppressHydrationWarning
+        >
+          <RandomReveal
+            isPlaying
+            duration={2}
+            revealDuration={0.3}
+            characters="REiZ"
+            onComplete={() => ({ shouldRepeat: true, delay: 5 })}
+          />
+        </h1>
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, type: "" }}
+        className="flex flex-col space-y-3 w-fit absolute md:top-1/2 md:left-[10%] xl:left-[20%] md:transform md:-translate-y-1/2 top-10 left-5"
+      >
+        <div className="text-gray-200 text-lg">{`[ BROWSE ]`}</div>
+
+        <Select value={selectedLanguage} onValueChange={setSelectedLanguage}>
+          <SelectTrigger className="text-gray-400 text-md border-none focus:ring-0 w-32">
+            <SelectValue placeholder="Language" />
+          </SelectTrigger>
+          <SelectContent className="overflow-scroll">
+            <SelectGroup>
+              <SelectLabel>Language</SelectLabel>
+              <SelectItem value="any">Any</SelectItem>
+              <SelectItem value="german">Deutsch</SelectItem>
+              <SelectItem value="english">English</SelectItem>
+              <SelectItem value="spanish">Español</SelectItem>
+              <SelectItem value="french">Français</SelectItem>
+              <SelectItem value="italian">Italiano</SelectItem>
+              <SelectItem value="portugese">Português</SelectItem>
+              <SelectItem value="russian">Pусский</SelectItem>
+              <SelectItem value="albanian">Shqip</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select value={selectedGenre} onValueChange={setSelectedGenre}>
+          <SelectTrigger className="text-gray-400 text-md border-none focus:ring-0 w-32">
+            <SelectValue placeholder="Genre"></SelectValue>
+          </SelectTrigger>
+          <SelectContent className="overflow-scroll">
+            <SelectGroup>
+              <SelectLabel>Genres</SelectLabel>
+              <SelectItem value="any">Any</SelectItem>
+              <SelectItem value="dance">Dance</SelectItem>
+              <SelectItem value="hip hop">Hip Hop</SelectItem>
+              <SelectItem value="hits">Hits</SelectItem>
+              <SelectItem value="house">House</SelectItem>
+              <SelectItem value="lofi">LoFi</SelectItem>
+              <SelectItem value="pop">Pop</SelectItem>
+              <SelectItem value="rock">Rock</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        {stations.length !== 0 && (
+          <Select
+            value={currentStation?.urlResolved}
+            onValueChange={(e) =>
+              selectStation(
+                stations.find((station) => station.urlResolved === e)!
+              )
+            }
           >
-            <DarkSide />
-          </motion.div>
-        ) : (
-          <div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1, type: "keyframes" }}
-            >
-              <AudioVizualizerBlob
-                stationURL={currentStation?.urlResolved || null}
-              />
-              {/* <AsciiEffectScene /> */}
-            </motion.div>
-
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1, type: "tween" }}
-              onClick={() => setShowEgg((prev) => !prev)}
-              className="hidden md:block absolute top-0 left-1/2 transform -translate-x-1/2 mt-20"
-            >
-              <h1
-                className="text-4xl text-gray-200 font-bold mb-6"
-                suppressHydrationWarning
-              >
-                <RandomReveal
-                  isPlaying
-                  duration={2}
-                  revealDuration={0.3}
-                  characters="REiZ"
-                  onComplete={() => ({ shouldRepeat: true, delay: 5 })}
-                />
-              </h1>
-            </motion.div>
-
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1, type: "" }}
-              className="flex flex-col space-y-3 w-fit absolute md:top-1/2 md:left-[10%] xl:left-[20%] md:transform md:-translate-y-1/2 top-10 left-5"
-            >
-              <div className="text-gray-200 text-lg">{`[ BROWSE ]`}</div>
-
-              <Select
-                value={selectedLanguage}
-                onValueChange={setSelectedLanguage}
-              >
-                <SelectTrigger className="text-gray-400 text-md border-none focus:ring-0 w-32">
-                  <SelectValue placeholder="Language" />
-                </SelectTrigger>
-                <SelectContent className="overflow-scroll">
-                  <SelectGroup>
-                    <SelectLabel>Language</SelectLabel>
-                    <SelectItem value="any">Any</SelectItem>
-                    <SelectItem value="german">Deutsch</SelectItem>
-                    <SelectItem value="english">English</SelectItem>
-                    <SelectItem value="spanish">Español</SelectItem>
-                    <SelectItem value="french">Français</SelectItem>
-                    <SelectItem value="italian">Italiano</SelectItem>
-                    <SelectItem value="portugese">Português</SelectItem>
-                    <SelectItem value="russian">Pусский</SelectItem>
-                    <SelectItem value="albanian">Shqip</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-
-              <Select value={selectedGenre} onValueChange={setSelectedGenre}>
-                <SelectTrigger className="text-gray-400 text-md border-none focus:ring-0 w-32">
-                  <SelectValue placeholder="Genre"></SelectValue>
-                </SelectTrigger>
-                <SelectContent className="overflow-scroll">
-                  <SelectGroup>
-                    <SelectLabel>Genres</SelectLabel>
-                    <SelectItem value="any">Any</SelectItem>
-                    <SelectItem value="dance">Dance</SelectItem>
-                    <SelectItem value="hip hop">Hip Hop</SelectItem>
-                    <SelectItem value="hits">Hits</SelectItem>
-                    <SelectItem value="house">House</SelectItem>
-                    <SelectItem value="lofi">LoFi</SelectItem>
-                    <SelectItem value="pop">Pop</SelectItem>
-                    <SelectItem value="rock">Rock</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-
-              {stations.length !== 0 && (
-                <Select
-                  value={currentStation?.urlResolved}
-                  onValueChange={(e) =>
-                    selectStation(
-                      stations.find((station) => station.urlResolved === e)!
-                    )
-                  }
-                >
-                  <SelectTrigger className="mb-4 my-3 text-gray-400 text-md w-32 line-clamp-1 whitespace-nowrap border-none focus:ring-0">
-                    <SelectValue placeholder="Stations" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Available Stations</SelectLabel>
-                      {stations.map((station, i) => (
-                        <SelectItem
-                          key={station.id}
-                          value={station.urlResolved}
-                        >
-                          {/* {`Station #${i + 1}`} */}
-                          {station.name}
-                        </SelectItem>
-                      ))}
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              )}
-            </motion.div>
-
-            {currentStation && (
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1, type: "tween" }}
-                className="absolute md:top-1/2 md:right-[10%] xl:right-[20%] md:transform md:-translate-y-1/2 top-10 right-5"
-              >
-                <div className="flex flex-col gap-3 text-gray-400 text-md overflow-x-hidden">
-                  <div className="text-gray-200">{`[ NOW PLAYING ]`}</div>
-                  <div className="relative flex items-center justify-between w-36">
-                    <div className="whitespace-nowrap animate-marquee">
-                      {currentStation.name}
-                    </div>
-                  </div>
-                  <div className="flex gap-3 items-center">
-                    <span>
-                      <ReactCountryFlag
-                        countryCode={currentStation.countryCode}
-                        svg
-                        aria-label={currentStation.country}
-                        title={currentStation.countryCode}
-                      />
-                    </span>
-                    <span className="whitespace-nowrap line-clamp-1 w-32">
-                      {currentStation.country.length > 16
-                        ? currentStation.countryCode
-                        : currentStation.country}
-                    </span>
-                  </div>
-                  <div>{`${currentStation.bitrate} kbps / ${currentStation.codec}`}</div>
-                  <a href={currentStation.homepage} target="_blank">
-                    Go to website
-                  </a>
-                </div>
-              </motion.div>
-            )}
-          </div>
+            <SelectTrigger className="mb-4 my-3 text-gray-400 text-md w-32 line-clamp-1 whitespace-nowrap border-none focus:ring-0">
+              <SelectValue placeholder="Stations" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Available Stations</SelectLabel>
+                {stations.map((station, i) => (
+                  <SelectItem key={station.id} value={station.urlResolved}>
+                    {/* {`Station #${i + 1}`} */}
+                    {station.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         )}
-      </AnimatePresence>
+      </motion.div>
+
+      {currentStation && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, type: "tween" }}
+          className="absolute md:top-1/2 md:right-[10%] xl:right-[20%] md:transform md:-translate-y-1/2 top-10 right-5"
+        >
+          <div className="flex flex-col gap-3 text-gray-400 text-md overflow-x-hidden">
+            <div className="text-gray-200">{`[ NOW PLAYING ]`}</div>
+            <div className="relative flex items-center justify-between w-36">
+              <div className="whitespace-nowrap animate-marquee">
+                {currentStation.name}
+              </div>
+            </div>
+            <div className="flex gap-3 items-center">
+              <span>
+                <ReactCountryFlag
+                  countryCode={currentStation.countryCode}
+                  svg
+                  aria-label={currentStation.country}
+                  title={currentStation.countryCode}
+                />
+              </span>
+              <span className="whitespace-nowrap line-clamp-1 w-32">
+                {currentStation.country.length > 16
+                  ? currentStation.countryCode
+                  : currentStation.country}
+              </span>
+            </div>
+            <div>{`${currentStation.bitrate} kbps / ${currentStation.codec}`}</div>
+            <a href={currentStation.homepage} target="_blank">
+              Go to website
+            </a>
+          </div>
+        </motion.div>
+      )}
     </div>
   );
 }
